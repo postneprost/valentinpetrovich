1b731b9565bd8ba50e309e32911beca21e0cfb97
postneprost <postneprost@yandex.ru>
2021-09-14 22:35:54 +0500

    Вынес функции

--- docs/script.js
+++ docs/script.js
@@ -6,34 +6,34 @@ window.onload = function(){
         page.classList.toggle("blood-theme");
       };
 
-      function getDigit(i) {
-        let num;
-        do {
-          num = prompt(`Введите число ${i}`, 0);
-        } while (!isFinite(num) ||  num === '');
-        return +num;
-      };
-
-      function Calculator() {
-
-        this.read = function() {
-          this.x = getDigit(1);
-          this.y = getDigit(2);
-        },
-
-        this.sum = function() {
-          return this.x + this.y;
-        },
-
-        this.mul = function() {
-          return this.x * this.y;
-        }
-      };
-
     let buttonCalculate = document.querySelector(".button-calculate");
     buttonCalculate.onclick = function() {
       let calculator = new Calculator();
       calculator.read();
       alert("Сумма = " + calculator.sum() + "\r" + "Произведение = " + calculator.mul());
-    }
+      }
+};
+
+function getDigit(i) {
+  let num;
+  do {
+    num = prompt(`Введите число ${i}`, 0);
+  } while (!isFinite(num) ||  num === '');
+  return +num;
+};
+
+function Calculator() {
+
+  this.read = function() {
+    this.x = getDigit(1);
+    this.y = getDigit(2);
+  },
+
+  this.sum = function() {
+    return this.x + this.y;
+  },
+
+  this.mul = function() {
+    return this.x * this.y;
+  }
 };
