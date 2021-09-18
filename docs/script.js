1a9b5ce78daef4a847b8524498994dfb071b963d
postneprost <postneprost@yandex.ru>
2021-09-19 00:25:59 +0500

    add get symbol count

--- docs/script.js
+++ docs/script.js
@@ -1,50 +1,53 @@
 window.onload = function() {
-    let page = document.querySelector(".page");
-    let buttonTheme = document.querySelector(".button-theme");
-    buttonTheme.onclick = function() {
-        page.classList.toggle("sea-theme");
-        page.classList.toggle("blood-theme");
-      };
+  let page = document.querySelector(".page");
+  let buttonTheme = document.querySelector(".button-theme");
+  buttonTheme.onclick = function() {
+      page.classList.toggle("sea-theme");
+      page.classList.toggle("blood-theme");
+    };
 
-    let buttonCalculate = document.querySelector(".button-calculate");
-    buttonCalculate.onclick = function() {
-      let calculator = new Calculator();
-      calculator.read();
-      alert("Сумма = " + calculator.sum() + "\r" + "Произведение = " + calculator.mul());
-      }
+  let buttonCalculate = document.querySelector(".button-calculate");
+  buttonCalculate.onclick = function() {
+    let calculator = new Calculator();
+    calculator.read();
+    alert("Сумма = " + calculator.sum() + "\r" + "Произведение = " + calculator.mul());
+    }
 
-    let buttonSymbolCode = document.querySelector(".button-symbol-code");
-    buttonSymbolCode.onclick = getSymbolCode;
+  let buttonSymbolCode = document.querySelector(".button-symbol-code");
+  buttonSymbolCode.onclick = getSymbolCode;
 
-    let buttonSymbolCount = document.querySelector(".button-symbol-count");
-    buttonSymbolCount.onclick = getSymbolCount;
+  let buttonSymbolCount = document.querySelector(".button-symbol-count");
+  buttonSymbolCount.onclick = getSymbolCount;
 
-    let buttonForArray = document.querySelector(".button-for-array");
-    buttonForArray.onclick = on;
+  let buttonWordLength = document.querySelector(".button-word-length");
+  buttonWordLength.onclick = sortWordLength;
+
+  let buttonPopWords = document.querySelector(".button-popular-symbol");
+  buttonPopWords.onclick = popWords;
 }
 
 function getDigit(i) {
-  let num;
-  do {
-    num = prompt(`Введите число ${i}`, 0);
-  } while (isNaN(parseFloat(num)) || !isFinite(num));
-  return +num;
+let num;
+do {
+  num = prompt(`Введите число ${i}`, 0);
+} while (isNaN(parseFloat(num)) || !isFinite(num));
+return +num;
 };
 
 function Calculator() {
 
-  this.read = function() {
-    this.x = getDigit(1);
-    this.y = getDigit(2);
-  },
+this.read = function() {
+  this.x = getDigit(1);
+  this.y = getDigit(2);
+},
 
-  this.sum = function() {
-    return this.x + this.y;
-  },
+this.sum = function() {
+  return this.x + this.y;
+},
 
-  this.mul = function() {
-    return this.x * this.y;
-  }
+this.mul = function() {
+  return this.x * this.y;
+}
 }
 
 function getSymbolCode() {
@@ -65,27 +68,40 @@ function getSymbolCount() {
 
   let count = 0;
   let pos = -1;
-  while ((pos = str.indexOf(symb, pos + 1)) != -1) {
-    count++;
-  }
+  while ((pos = str.indexOf(symb, pos + 1)) != -1) {count++;}
   alert(`Количество символов '${symb}' в строке: ${count}`);
 }
 
-function getUserText() {
-  return document.querySelector(".array-textarea").value;
-}
+let getUserText = () => document.querySelector(".array-textarea").value;
 
-function sortArrayByLengthWord(arr) {
-  return arr.split(' ').slice().sort((a, b) => b.length - a.length);
-}
+let sortArrayByLengthWord = (arr) => arr.split(' ').slice().sort((a, b) => b.length - a.length);
 
-function arrayToText(arr) {
-  return arr.join('\t');
-}
+let arrayToText = (arr) => arr.join('\t');
 
-function on() {
+function sortWordLength() {
   overlay.style.display = "block";
   document.querySelector(".lightbox-content").textContent = arrayToText(sortArrayByLengthWord(getUserText()));
+}
+
+function getPopularSymbols(str) {
+  let popSymbolsObj = {};
+  let resStr = '';
+
+  str.split('').forEach((item) => {
+      !popSymbolsObj[item] ? popSymbolsObj[item] = 1 : popSymbolsObj[item]++;
+    }
+  );
+
+  Object.entries(popSymbolsObj).sort((a, b) => b[1] - a[1]).forEach((item) => {
+    resStr += `${item[0]}: ${item[1]}\n\r`;
+  });
+
+  return resStr;
+}
+
+function popWords() {
+  overlay.style.display = "block";
+  document.querySelector(".lightbox-content").textContent = getPopularSymbols(getUserText());
 }
 
 function off() {
